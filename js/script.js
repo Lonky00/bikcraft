21b7f401d8cf549fec7f1c5a151d5227ba669721
pragma <diogosouza03@gmail.com>
2023-07-17 13:56:52 -0400

    parametros

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar os links do menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link){
@@ -5,8 +6,24 @@ function ativarLink(link){
     const href = link.href;
     if (url.includes(href)){
         link.classList.add("ativo");
-        console.log(href);
+        
     }
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProdutos(parametro){
+    const elemento = document.getElementById(parametro);
+    if (elemento){
+        elemento.checked = true;
+    }
+    console.log(parametro);
+    console.log(elemento);
+    
+}
+
+parametros.forEach(ativarProdutos);
